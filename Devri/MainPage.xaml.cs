be88daa650f6bdff5f6e49e279b4bd2480882d8e
YonghoAhn <nanhosoft@gmail.com>
2017-10-17 09:29:10 +0900

    BindablePage

--- Devri/MainPage.xaml.cs
+++ Devri/MainPage.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using Devri.Common;
-using static Devri.Common.BindablePage;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -28,21 +27,8 @@ namespace Devri
     /// <summary>
     /// 자체적으로 사용하거나 프레임 내에서 탐색할 수 있는 빈 페이지입니다.
     /// </summary>
-    public sealed partial class MainPage : Page, INotifyPropertyChanged
+    public sealed partial class MainPage : BindablePage
     {
-        public event PropertyChangedEventHandler PropertyChanged = null;
-        private void OnPropertyChanged([CallerMemberName]string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-        private bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
-        {
-            if (Equals(storage, value)) return false;
-
-            storage = value;
-            OnPropertyChanged(propertyName);
-            return true;
-        }
         private float m_temperature;        
         public float Temperature
         {
