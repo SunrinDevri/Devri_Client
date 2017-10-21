c6d6b124185e5b935e9e5854e72574229318517e
SoengMin Lee <zoq2039@gmail.com>
2017-10-22 03:51:32 +0900

    Teaching Feeling

---
+++ Devri/Common/Feeling.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Devri.Common
+{
+    class Feeling
+    {
+        int XAxis;
+        int YAxis;
+        public void SetXAxis(int distance)
+        { }
+        public void SetYAxis(int distance)
+        { }
+        public int GetXAxis()
+        { return XAxis; }
+        public int GetYAxis()
+        { return YAxis; }
+    }
+}
