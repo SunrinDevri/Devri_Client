bd009628d42e11759eacbe2117379e9a80c320cb
SoengMin Lee <zoq2039@gmail.com>
2017-11-02 07:08:08 +0900

    dd

--- Devri/Common/Feeling.cs
+++ Devri/Common/Feeling.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace Devri.Common
 {
     class Feeling
     {
-        int XAxis;
-        int YAxis;
+        public int XAxis;
+        public int YAxis;
+        public int Status; // 0 = Normal 1=Positive 2=Angry 3=Sad 
         public void SetXAxis(int distance)
-        { }
+        {
+        }
         public void SetYAxis(int distance)
-        { }
+        {
+        }
         public int GetXAxis()
         { return XAxis; }
         public int GetYAxis()
         { return YAxis; }
+        Feeling()
+        {
+        }
+        public void InitializeFeel()
+        {
+            XAxis = 0;
+            YAxis = 0;
+        }
+
     }
 }
